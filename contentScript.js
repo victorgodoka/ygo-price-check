b1ff34fa8d92723460c796b6d433b966986a4eda
Victor Martins <martinsvictorsantos@gmail.com>
2023-09-21 15:26:32 -0300

    correção no manifest e no html

--- contentScript.js
+++ contentScript.js
@@ -12,7 +12,7 @@
           card_prices
         } = data[0]
 
-        const element = `<div class="precos-edicoes" align="center">
+        const element = `<div class="" align="center">
   <div
     class="row bloco-preco-superior"
     title="Preço Médio card Normal (Sem extras)"
@@ -20,7 +20,7 @@
     <div class="col-ext"><div class="avgp-extra">TCGPLAYER</div></div>
     <div class="col-prc col-prc-menor"> </div>
     <div class="col-sep"><div class="circle"></div></div>
-    <div class="col-prc col-prc-menor"font-size: 1.255rem">$ ${(card_prices[0].tcgplayer_price).toFixed(2)}</div>
+    <div class="col-prc col-prc-menor"font-size: 1.255rem">$ ${card_prices[0].tcgplayer_price}</div>
     <div class="col-sep"><div class="circle"></div></div>
     <div class="col-prc col-prc-maior">R$ ${(card_prices[0].tcgplayer_price * 5).toFixed(2)}</div>
   </div>
@@ -31,7 +31,7 @@
     <div class="col-ext"><div class="avgp-extra avgp-extra-29">CARDMARKET</div></div>
     <div class="col-prc col-prc-menor"> </div>
     <div class="col-sep"><div class="circle"></div></div>
-    <div class="col-prc col-prc-menor"font-size: 1.255rem">€ ${(card_prices[0].cardmarket_price).toFixed(2)}</div>
+    <div class="col-prc col-prc-menor"font-size: 1.255rem">€ ${card_prices[0].cardmarket_price}</div>
     <div class="col-sep"><div class="circle"></div></div>
     <div class="col-prc col-prc-maior">R$ ${(card_prices[0].cardmarket_price * 5.25).toFixed(2)}</div>
   </div>

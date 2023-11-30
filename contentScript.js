c3310f4ec96e6e0637949b8c8ea77dc11b19dc7a
Victor Martins <martinsvictorsantos@gmail.com>
2023-11-30 09:51:41 -0300

    att: preços acompanham a edição selecionada

--- contentScript.js
+++ contentScript.js
@@ -1,17 +1,37 @@
-(async function(){
-  const englishNameElement = document.querySelector('.nome-auxiliar');
-  const portugueseNameElement = document.querySelector('.nome-principal span');
+window.addEventListener("load", async (event) => {
+  const englishNameElement = document.querySelector(".nome-auxiliar");
+  const portugueseNameElement = document.querySelector(".nome-principal span");
+  const findEdicao = () => document.querySelector("li.ed-sel font").innerText
 
   if (englishNameElement && portugueseNameElement) {
-    const quote = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL");
+    const quote = await fetch(
+      "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL"
+    );
     const { USDBRL, EURBRL } = await quote.json();
     const { ask: dollar } = USDBRL;
     const { ask: euro } = EURBRL;
 
-    const cardName = englishNameElement.textContent || portugueseNameElement.textContent;
-    const res = await fetch(`https://db.ygoprodeck.com/api/v7/cardinfo.php?name=${encodeURIComponent(cardName)}`);
+    const cardName =
+      englishNameElement.textContent || portugueseNameElement.textContent;
+    const res = await fetch(
+      `https://db.ygoprodeck.com/api/v7/cardinfo.php?name=${encodeURIComponent(
+        cardName
+      )}&tcgplayer_data`
+    );
     const { data } = await res.json();
     const { card_prices } = data[0];
+    console.log(data[0]);
+
+    Array.from(document.querySelectorAll("li[id^=edcard")).forEach((el) => {
+      el.addEventListener("mouseover", () => {
+        const price = data[0].card_sets
+          .sort((b, a) => b.set_price - a.set_price)
+          .find(({ set_code }) => set_code === findEdicao())?.set_price;
+        if (!price) return;
+        document.querySelector("#precousd").innerHTML = '$ ' + (price)
+        document.querySelector("#precousdconvertido").innerHTML = 'R$ ' + (price * dollar).toFixed(2)
+      });
+    });
 
     const element = `<div class="" align="center">
   <div
@@ -21,9 +41,13 @@
     <div class="col-ext"><div class="avgp-extra">TCGPLAYER</div></div>
     <div class="col-prc col-prc-menor"> </div>
     <div class="col-sep"><div class="circle"></div></div>
-    <div class="col-prc col-prc-menor"font-size: 1.255rem">$ ${card_prices[0].tcgplayer_price}</div>
+    <div id="precousd" class="col-prc col-prc-menor">$ ${
+      card_prices[0].tcgplayer_price
+    }</div>
     <div class="col-sep"><div class="circle"></div></div>
-    <div class="col-prc col-prc-maior">R$ ${(card_prices[0].tcgplayer_price * dollar).toFixed(2)}</div>
+    <div id="precousdconvertido" class="col-prc col-prc-maior">R$ ${(
+      card_prices[0].tcgplayer_price * dollar
+    ).toFixed(2)}</div>
   </div>
   <div
     class="row bloco-preco-superior"
@@ -32,16 +56,19 @@
     <div class="col-ext"><div class="avgp-extra avgp-extra-29">CARDMARKET</div></div>
     <div class="col-prc col-prc-menor"> </div>
     <div class="col-sep"><div class="circle"></div></div>
-    <div class="col-prc col-prc-menor"font-size: 1.255rem">€ ${card_prices[0].cardmarket_price}</div>
+    <div class="col-prc col-prc-menor">€ ${
+      card_prices[0].cardmarket_price
+    }</div>
     <div class="col-sep"><div class="circle"></div></div>
-    <div class="col-prc col-prc-maior">R$ ${(card_prices[0].cardmarket_price * euro).toFixed(2)}</div>
+    <div class="col-prc col-prc-maior">R$ ${(
+      card_prices[0].cardmarket_price * euro
+    ).toFixed(2)}</div>
   </div>
 </div>
-
         `;
-    document.querySelector('.desktop-price-lines-0').innerHTML += element;
+    document.querySelector(".desktop-price-lines-0").innerHTML += element;
     console.log(data);
   } else {
-    console.log('Element with class "nome-auxiliar" not found')
+    console.log('Element with class "nome-auxiliar" not found');
   }
-})();
+});
